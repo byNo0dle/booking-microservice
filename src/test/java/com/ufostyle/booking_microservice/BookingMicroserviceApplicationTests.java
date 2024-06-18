c020ec9e03e6e64609a1a6d4d7e9bbe31306f0f1
Nick Saurino <nicksaurinocruz@gmail.com>
2024-06-18 16:13:04 -0500

    Modify the properties by the yml

---
+++ src/test/java/com/ufostyle/booking_microservice/BookingMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufostyle.booking_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
